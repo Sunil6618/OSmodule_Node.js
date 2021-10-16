079ad387e699426067ea2f01dcc67562d707da5e
SUNIL SHAHU <61456130+Sunil6618@users.noreply.github.com>
2021-10-16 23:30:58 +0530

    Create Assign2.js

---
+++ Assign2.js
@@ -0,0 +1,6 @@
+var os=require("os");
+console.log("Architecture" + os.arch());
+console.log("Free memory of the System" + os.freemem());
+console.log("Total memory of the System" + os.totalmem());
+console.log("Plateform" + os.platform());
+console.log("Current User" + os.userInfo());
